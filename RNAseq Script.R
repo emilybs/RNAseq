a817d231f10fa4f740cca97523e99f55c62ce812
Emily Beckett Sward <emilybeckettsward@Emilys-MacBook-Pro.local>
2018-05-11 11:28:10 -0400

    Installed Bioconductor

--- RNAseq Script.R
+++ RNAseq Script.R
@@ -1,1 +1,16 @@
-#Hello Emily!
+
+# Description -------------------------------------------------------------
+# This script was written by Emily Beckett as part of her PhD project
+
+
+
+# Packages ----------------------------------------------------------------
+## try http:// if https:// URLs are not supported
+source("https://bioconductor.org/biocLite.R")
+biocLite()
+
+
+
+# Import Data -------------------------------------------------------------
+
+
