65b35265089ae3963c5732f9633fae27cb92ac9e
Emily Beckett Sward <emilybeckettsward@Emilys-MacBook-Pro.local>
2018-05-14 08:27:05 -0400

    added walkthrough from RNA-seq workflow and the tximport

---
+++ tximport.R
@@ -0,0 +1,7 @@
+################################################################################################################################################
+# Importing transcript abundance data and creating DESeqDataSet
+# Code modelled after: https://bioconductor.org/packages/release/bioc/vignettes/tximport/inst/doc/tximport.html
+# The above link is recommended here: https://master.bioconductor.org/packages/release/workflows/vignettes/rnaseqGene/inst/doc/rnaseqGene.html
+################################################################################################################################################
+
+
